bedba6c85ee8359293218586b844fbca705c4d02
marloderik <marloderik@hotmail.com>
2015-03-24 08:28:08 -0400

    Create spacewar_temp_ing.js

---
+++ content/temp_json/spacewar_temp_ing.js
@@ -0,0 +1,51 @@
+ 'type': ' Space Shotter', // game or console
+        'name': 'Space War',
+        'authors': ['Steve Russel', 'Dan Edwards','Alan Kotok','Peter Sampson','Wayne Wiitanen Graetz'],
+        'company': [' Massachusetts Institute of Technology(MIT)'],  
+        'year': '1961',
+        'date': '30-07-1961', // Day, Month, Year
+        'headline': 'First Game about space',
+        'resume': [
+            ' Spacewar is one of the earliest digital computer video games , it is a two-player game , with each player taking control of a  ',
+            ' starship and attempting to destroy the other. A star in the center of the screen pulls on both ships and requires maneuvering  ',
+            ' to avoid falling into it.In an emergency,a player can enter hyperspace to return at a random location on the screen, but only .'
+            'at the risk of exploding if it is exploited.'
+            'After Alan Kotok obtained some sine and cosine routines from DEC, Russell began,coding, and, by February 1962, produced its first version.'
+            'It took about 200 hours of work to create the initial version.The additional features were developed by Dan Edwards,Peter Samson, and Graetz.'
+        ].join(''),
+        'full_text': [
+            'Tudo começou como um desafio para Russell: com o novo computador dotado de transistores ao invés de válvulas e uma tela  ',
+            '(peças de luxo na época), o TX-0, conhecido como Tixo, eles decidiram fazer',
+            'o que ninguém mais fez por eles: transpor a ficção científica da literatura para uma outra mídia. ',
+            'É verdade que o que eles queriam mesmo era um filme, mas isso é outra história. Infelizmente, eles não tinham  ',
+            'acesso à enorme máquina.',
+            ' O TX-0 e igualmente programável, ele era o sonho dos garotos. E poderia ser ligado ',
+' instantâneamente, ao contrário de seu predecessor,, que precisava de uma manhã inteira para aquecimento  ',
+            'interno. Eles queriam criar algum tipo de demonstração, e por isso criaram algumas regras que seu programa deveria seguir: ',
+            '1.Deveria demonstrar as capacidades do computador, usando quase todo seu potencial;',
+            '2.Deveria ser interessante e interativo (diferente toda vez que rodado);'
+'3.Deveria envolver o usuário de maneira atrativa e prazerosa - ou seja, deveria ser um jogo.'
+'Os jovens então criaram duas naves, uma rotina para simular inércia  '
+'e um campo estelar aleatório para ajudar a controlar o movimento. Elesqueriam também adicionar um "botão de pânico para emergências",'
+' e assim nasceu a tecla Hiper-Espaço. O último toque seria a estrela no meio do campo de batalha, que gerava um campo gravitacional '
+' que poderia tanto atrapalhar ou ajudar, dependendo da astúcia do jogador.'
+'Spacewar! só estaria oficialmente finalizado em 1962, ocupando míseros 2KB…Russell jamais ganharia um dólar pelo jogo '
+'- mas isso não quer dizer que outros não lucrariam com ele.'
+        ].join(''),
+        'thumbnail': 'thumbnail_example.png',
+        'main_img': {'name': 'main_picture.png', 'caption': "description"},
+        'images': [
+            {'name': 'picture1.png', 'caption': "description1"},
+            {'name': 'picture2.png', 'caption': "description2"},
+            {'name': 'picture3.png', 'caption': "description3"}
+        ],
+        'video_gameplay': [
+            {'url': 'video_url', 'caption': "description", 'type': "youtube"} // type: youtube or vimeo
+        ],
+        'demos': [
+            {'url': 'demonstration_url', 'caption': "description"}
+        ],
+        'references': [
+            {'url': 'reference_url1', 'caption': "description"},
+            {'url': 'reference_url2', 'caption': "description"},
+            {'url': 'reference_url3', 'caption': "description"}
